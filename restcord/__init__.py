027a61ecd79e2129667da48c3f1ae3e36991bfe3
JustMaffie <jorivanee@hotmail.com>
2017-08-27 13:44:28 +0200

    Bump version

--- restcord/__init__.py
+++ restcord/__init__.py
@@ -17,5 +17,5 @@ __license__ = 'MIT'
 __copyright__ = 'Copyright 2015-2017 JustMaffie'
 __major__ = "0"
 __minor__ = "1"
-__patch__ = "0"
+__patch__ = "1"
 __version__ = "{}.{}.{}".format(__major__, __minor__, __patch__)
